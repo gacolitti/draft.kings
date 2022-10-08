2c8d277c467c541afbb9d4dce394afd6e3760a00
Giovanni Colitti <g.a.colitti@gmail.com>
2022-10-08 15:41:14 -0500

    Add get_draft_group_info() and rename old function to get_draftable_players()

--- R/draftgroup.R
+++ R/draftgroup.R
@@ -1,23 +1,8 @@
 
-#' Get Draft Group Info
+#' Check Draft Group Inputs
 #'
-#' Fetch information on the available lineup for a specific
-#'   competition. In particular, this includes player names/IDs and
-#'   their salaries.
-#'
-#' @inheritParams get_contest_info
-#'
-#' @param draft_group_id Sequence of digits that correspond to a draft table/group.
-#'   If `draft_group_id` and `contest_id` are both passed, `contest_id` is ignored.
-#'
-#' @examples
-#'   \dontrun{
-#'     get_draft_group_info(75367)
-#'   }
-#'
-#' @export
-get_draft_group_info <- function(draft_group_id = NULL,
-                            contest_id = NULL) {
+#' Use either `draft_group_id` or `contest_id` if `draft_group_id` is `NULL`.
+check_draft_group_id <- function(draft_group_id, contest_id) {
 
   # Ensure at least one argument is passed
   if (all(is.null(draft_group_id), is.null(contest_id))) {
@@ -35,6 +20,32 @@ get_draft_group_info <- function(draft_group_id = NULL,
 
   }
 
+  draft_group_id
+
+}
+
+#' Get Draftable Players
+#'
+#' Fetch information on the available lineup for a specific
+#'   competition. In particular, this includes player names/IDs and
+#'   their salaries.
+#'
+#' @inheritParams get_contest_info
+#'
+#' @param draft_group_id Sequence of digits that correspond to a draft table/group.
+#'   If `draft_group_id` and `contest_id` are both passed, `contest_id` is ignored.
+#'
+#' @examples
+#'   \dontrun{
+#'     get_draft_group_info(75367)
+#'   }
+#'
+#' @export
+get_draftable_players <- function(draft_group_id = NULL,
+                            contest_id = NULL) {
+
+  draft_group_id <- check_draft_group_id(draft_group_id, contest_id)
+
   draft_groups_json <- httr2::request(
     glue::glue("https://api.draftkings.com/draftgroups/v1/draftgroups/{draft_group_id}/draftables")
   ) %>%
@@ -48,6 +59,8 @@ get_draft_group_info <- function(draft_group_id = NULL,
     clean_names()
 
 }
+
+#' Get Draft Group Info
 
 #' Get List of Draft Groups
 #'
@@ -68,6 +81,34 @@ get_draft_groups <- function() {
 
 }
 
+#' Get Draft Group Info
+#'
+#' Fetch info for a specific draft group
+#'
+#' @inheritParams get_draftable_players
+#'
+#' @export
+get_draft_group_info <- function(draft_group_id = NULL,
+                                 contest_id = NULL) {
+
+  draft_group_id <- check_draft_group_id(draft_group_id, contest_id)
+
+  res <- httr2::request(
+    glue::glue("https://api.draftkings.com/draftgroups/v1/{draft_group_id}")
+  ) %>%
+    httr2::req_perform() %>%
+    httr2::resp_body_string()
+
+  res %>%
+    tidyjson::enter_object("draftGroup") %>%
+    tidyjson::spread_all() %>%
+    dplyr::as_tibble() %>%
+    dplyr::select(-document.id) %>%
+    clean_names()
+
+
+}
+
 #' Get Player List
 #'
 #' Fetch list of players for a specific draft group and related info
@@ -78,21 +119,7 @@ get_draft_groups <- function() {
 get_player_list <- function(draft_group_id = NULL,
                             contest_id = NULL) {
 
-  # Ensure at least one argument is passed
-  if (all(is.null(draft_group_id), is.null(contest_id))) {
-
-    cli::cli_abort(
-      "`draft_group_id` and `contest_id` cannot both be missing"
-    )
-
-  }
-
-  # Get draft_group_id when it is missing
-  if (is.null(draft_group_id)) {
-
-    draft_group_id <- get_contest(contest_id)$draft_group_id
-
-  }
+  draft_group_id <- check_draft_group_id(draft_group_id, contest_id)
 
   res <- httr2::request("https://www.draftkings.com/lineup/getavailableplayers") %>%
     httr2::req_url_query(draftGroupId = draft_group_id) %>%
@@ -117,21 +144,7 @@ get_player_list <- function(draft_group_id = NULL,
 get_team_list <- function(draft_group_id = NULL,
                             contest_id = NULL) {
 
-  # Ensure at least one argument is passed
-  if (all(is.null(draft_group_id), is.null(contest_id))) {
-
-    cli::cli_abort(
-      "`draft_group_id` and `contest_id` cannot both be missing"
-    )
-
-  }
-
-  # Get draft_group_id when it is missing
-  if (is.null(draft_group_id)) {
-
-    draft_group_id <- get_contest(contest_id)$draft_group_id
-
-  }
+  draft_group_id <- check_draft_group_id(draft_group_id, contest_id)
 
   res <- httr2::request("https://www.draftkings.com/lineup/getavailableplayers") %>%
     httr2::req_url_query(draftGroupId = draft_group_id) %>%
