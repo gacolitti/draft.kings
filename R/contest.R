a1d6a16fdad64c1db4a33c93c9c1d700c8b75883
Giovanni Colitti <g.a.colitti@gmail.com>
2022-12-17 19:42:18 -0600

    [enhancement]: Use dkreq() and update get_gametype_rules() accordingly

--- R/contest.R
+++ R/contest.R
@@ -3,11 +3,7 @@
 #'
 #' Fetch contest information such as the sport, payout, and contest summary.
 #'
-#' @inheritParams add_proxy
-#' @inheritParams add_curl_options
-#' @inheritParams add_throttle
-#' @inheritParams add_headers
-#' @inheritParams httr2::req_error
+#' @inheritParams dkreq
 #' @param contest_id The sequence of digits that correspond to a specific contest.
 #'   This can be found by examining the URL of a contest page.
 #'   For example: \url{https://www.draftkings.com/draft/contest/133645678#}. Here the contest ID
@@ -24,7 +20,7 @@ get_contest_info <- function(contest_id,
                              proxy_args = NULL,
                              headers = NULL,
                              retry_options = NULL,
-                             is_error_func = NULL,
+                             error_handling_options = NULL,
                              curl_options = NULL,
                              output = c("cleaned_json", "json", "response", "request")) {
 
@@ -32,20 +28,14 @@ get_contest_info <- function(contest_id,
   output <- match.arg(output)
 
   req <- dkreq(
-    req,
     proxy_args = proxy_args,
     throttle_rate = throttle_rate,
     headers = headers,
     curl_options = curl_options,
     retry_options = retry_options,
-    is_error_func = is_error_func
-  )
-
-  req <- httr2::req_url_path_append(
-    req,
-    glue::glue(
-      "contests/v1/contests/{contest_id}?format=json"
-    )
+    error_handling_options = error_handling_options,
+    url_path = glue::glue("contests/v1/contests/{contest_id}"),
+    query_params = list(format = "json")
   )
 
   dkreq_process(req, output)
@@ -54,21 +44,34 @@ get_contest_info <- function(contest_id,
 
 #' Get List of Contests
 #'
-#' Fetch the full table of contests and related info
+#' Fetch the full table of contests and related info from DraftKings.com lobby
+#'
+#' @inheritParams dkreq
 #'
 #' @export
-get_contests <- function(sport = NULL) {
+get_contests <- function(sport = NULL,
+                         throttle_rate = NULL,
+                         proxy_args = NULL,
+                         headers = NULL,
+                         retry_options = NULL,
+                         error_handling_options = NULL,
+                         curl_options = NULL,
+                         output = c("cleaned_json", "json", "response", "request")) {
 
-  res <-   httr2::request(base_url = "https://www.draftkings.com/lobby/getcontests") %>%
-    httr2::req_url_query(sport = sport) %>%
-    httr2::req_perform() %>%
-    httr2::resp_body_json()
+  output <- match.arg(output)
 
-  res$Contests %>%
-    tidyjson::spread_all() %>%
-    dplyr::as_tibble() %>%
-    dplyr::select(-document.id) %>%
-    clean_names()
+  req <- dkreq(
+    base_url = "https://www.draftkings.com/",
+    throttle_rate = throttle_rate,
+    headers = headers,
+    curl_options = curl_options,
+    retry_options = retry_options,
+    error_handling_options = error_handling_options,
+    url_path = "lobby/getcontests",
+    query_params = list(sport = sport)
+  )
+
+  dkreq_process(req, output, subset = "Contests")
 
 }
 
@@ -79,10 +82,15 @@ get_contests <- function(sport = NULL) {
 #' @param game_type_id Integer corresponding to the game type.
 #'   For example, 159 in \url{https://api.draftkings.com/lineups/v1/gametypes/159/rules}.
 #'   If both `game_type_id` and `contest_id` are passed, then `contest_id` is ignored.
+#' @param ... Arguments passed to [dkreq()]
 #'
 #' @export
 get_gametype_rules <- function(game_type_id = NULL,
-                               contest_id = NULL) {
+                               contest_id = NULL,
+                               output = c("cleaned_json", "json", "response", "request"),
+                               ...) {
+
+  output <- match.arg(output)
 
   if (all(is.null(game_type_id), is.null(contest_id))) {
 
@@ -98,15 +106,25 @@ get_gametype_rules <- function(game_type_id = NULL,
 
   }
 
-  httr2::request(
-    glue::glue("https://api.draftkings.com/lineups/v1/gametypes/{game_type_id}/rules")
-  ) %>%
-    httr2::req_perform() %>%
-    httr2::resp_body_string() %>%
-    tidyjson::spread_all() %>%
+  req <- dkreq(..., url_path = glue::glue("lineups/v1/gametypes/{game_type_id}/rules"))
+
+  out <- dkreq_process(req, output = "json")
+
+  salary <- out$salaryCap %>%
+    purrr::compact() %>%
     dplyr::as_tibble() %>%
-    dplyr::select(-document.id) %>%
-    clean_names()
+    clean_names() %>%
+    setNames(paste0("salary_cap_", names(.)))
+
+  team_count <- out$teamCount %>%
+    purrr::compact() %>%
+    dplyr::as_tibble() %>%
+    clean_names() %>%
+    setNames(paste0("team_count_", names(.)))
+
+  unique_players <- dplyr::tibble("unique_players" = out$uniquePlayers)
+
+  dplyr::bind_cols(salary, team_count, unique_players)
 
 }
 
--- R/dkreq.R
+++ R/dkreq.R
@@ -1,13 +1,27 @@
 
-dkreq <- function(req,
-                  proxy_args = NULL,
+#' Create Draft Kings Request Object
+#'
+#' @inheritParams add_proxy
+#' @inheritParams add_curl_options
+#' @inheritParams add_throttle
+#' @inheritParams add_headers
+#' @inheritParams httr2::req_error
+#' @inheritParams httr2::request
+#'
+#' @param is_error_func See [httr2::req_error()]
+#' @param error_body_func See [httr2::req_error()]
+#'
+#' @export
+dkreq <- function(proxy_args = NULL,
                   curl_options = NULL,
                   throttle_rate = NULL,
                   headers = NULL,
                   retry_options = NULL,
-                  is_error_func = NULL,
-                  error_body_func = get_error_body,
-                  base_url = "https://api.draftkings.com/") {
+                  url_path = NULL,
+                  query_params = NULL,
+                  error_handling_options = NULL,
+                  base_url = "https://api.draftkings.com/"
+                  ) {
 
   req <- httr2::request(base_url)
   req <- add_proxy(req, proxy_args)
@@ -15,7 +29,9 @@ dkreq <- function(req,
   req <- add_throttle(req, throttle_rate)
   req <- add_headers(req, headers)
   req <- add_retry(req, retry_options)
-  req <- httr2::req_error(req, body = error_body_func, is_error = is_error_func)
+  req <- add_error_handling(req, error_handling_options)
+  req <- add_to_path(req, url_path)
+  req <- httr2::req_url_query(req, !!! query_params)
 
   req
 
